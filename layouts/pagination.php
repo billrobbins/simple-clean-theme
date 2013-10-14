dd9414521caddd857f9abdca171215b27f23b91c
Bill Robbins <bill@organizedthemes.com>
2013-10-14 10:49:14 -0500

    Updated Pagination

--- layouts/pagination.php
+++ layouts/pagination.php
@@ -1,2 +1,7 @@
+<nav class="pagination clearfix">
 		
+	<div class="left"><?php next_posts_link( esc_html__( '&lt;', 'organizedthemes' ) ); ?></div>
 	
+	<div class="right"><?php previous_posts_link( esc_html__( '&gt;', 'organizedthemes' ) ); ?></div>
+			
+</nav>
