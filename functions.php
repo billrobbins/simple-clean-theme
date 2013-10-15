690e81d057e1a6b7bfc910b0439f45030af939e2
Bill Robbins <bill@organizedthemes.com>
2013-10-15 13:34:01 -0500

    Updated Version Number

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
  */
 
 // Define Version
-	define( 'SIMPLE_VERSION', '1.0.0' );
+	define( 'SIMPLE_VERSION', '1.0.1' );
 
 // options framework
 	if ( !function_exists( 'optionsframework_init' ) ) {
