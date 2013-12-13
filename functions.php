72242eadd6beb0eebbd3b684b4e8206ac70bd5c1
Bill Robbins <bill@organizedthemes.com>
2013-12-12 20:18:34 -0600

    Revert "Updated Version Number"
    This reverts commit 71d742229144a5510fa963b4722225e617a4242f.

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
  */
 
 // Define Version
-	define( 'SIMPLE_VERSION', '1.0.4' );
+	define( 'SIMPLE_VERSION', '1.0.1' );
 
 // options framework
 	if ( !function_exists( 'optionsframework_init' ) ) {
